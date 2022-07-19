e55653c0ba5ed11e281fb7c6d2b134759cfe914f
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-18 20:09:59 -0400

    :construction: work

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< HEAD
 import { faBell, faInfoCircle, faClose } from '@fortawesome/free-solid-svg-icons';
+=======
+import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+>>>>>>> 0bd42fb... :construction: Work
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +13,10 @@ export class NavbarComponent implements OnInit {
 
   faBell = faBell;
   faInfoCircle = faInfoCircle;
+<<<<<<< HEAD
   faClose = faClose;
+=======
+>>>>>>> 0bd42fb... :construction: Work
 
   isOpen = false;
   isOpenBody = false;
