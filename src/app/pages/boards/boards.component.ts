09aa9cd43a36cf4181ae879c5b6908e979236c02
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-19 15:52:30 -0400

    :construction: Work dynamic accordions

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -19,6 +19,42 @@ export class BoardsComponent implements OnInit {
   faUsers = faUsers;
   faGear = faGear;
 
+  items = [
+    {
+      label: 'Item 1',
+      items: [
+        {
+          label: 'Sub Item 1.1'
+        },
+        {
+          label: 'Sub Item 1.2'
+        }
+      ]
+    },
+    {
+      label: 'Item 2',
+      items: [
+        {
+          label: 'Sub Item 2.1'
+        },
+      ]
+    },
+    {
+      label: 'Item 3',
+      items: [
+        {
+          label: 'Sub Item 3.1'
+        },
+        {
+          label: 'Sub Item 3.2'
+        },
+        {
+          label: 'Sub Item 3.3'
+        },
+      ]
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
