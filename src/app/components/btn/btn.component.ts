43bd2d75f1c83e758f6774662c79dfdc25b0a4a4
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-20 17:15:32 -0400

    :construction: work

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -7,7 +7,40 @@ import { Component, OnInit, Input } from '@angular/core';
 export class BtnComponent implements OnInit {
 
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
-  @Input() color = 'primary';
+  @Input() color: 'success' | 'primary' | 'danger' | 'light' | 'sky' = 'primary';
+
+  mapColors = {
+    success: {
+      'bg-success-700': true,
+      'hover:bg-success-800': true,
+      'focus:ring-success-300': true,
+      'text-white': true
+    },
+    primary: {
+      'bg-primary-700': true,
+      'hover:bg-primary-800': true,
+      'focus:ring-primary-300': true,
+      'text-white': true
+    },
+    danger: {
+      'bg-danger-700': true,
+      'hover:bg-danger-800': true,
+      'focus:ring-danger-300': true,
+      'text-white': true
+    },
+    sky: {
+      'bg-sky-700': true,
+      'hover:bg-sky-800': true,
+      'focus:ring-sky-300': true,
+      'text-white': true
+    },
+    light: {
+      'bg-gray-200': true,
+      'hover:bg-gray-500': true,
+      'focus:ring-gray-50': true,
+      'text-gray-700': true
+    }
+  }
 
   constructor() { }
 
@@ -15,22 +48,11 @@ export class BtnComponent implements OnInit {
   }
 
   get colors() {
-    return {
-      'text-white': this.color === 'success' || this.color === 'primary' || this.color === 'red',
-      'text-gray-700': this.color === 'gray-light',
-      'bg-success-700': this.color === 'success',
-      'hover:bg-success-800': this.color === 'success',
-      'focus:ring-success-300': this.color === 'success',
-      'bg-primary-700': this.color === 'primary',
-      'hover:bg-primary-800': this.color === 'primary',
-      'focus:ring-primary-300': this.color === 'primary',
-      'bg-red-700': this.color === 'red',
-      'hover:bg-red-800': this.color === 'red',
-      'focus:ring-red-300': this.color === 'red',
-      'bg-gray-200': this.color === 'gray-light',
-      'hover:bg-gray-500': this.color === 'gray-light',
-      'focus:ring-gray-50': this.color === 'gray-light',
-    };
+    const colors = this.mapColors[this.color];
+    if (colors) {
+      return colors;
+    }
+    return {};
   }
 
 }
--- src/app/pages/table/data-source.ts
+++ src/app/pages/table/data-source.ts
@@ -37,7 +37,11 @@ export class DataSourceProduct extends DataSource<Product> {
   }
 
   find(query: string) {
-    const newProducts = this.originalData.filter(item => item.title.toLowerCase().includes(query.toLowerCase()));
+    const newProducts = this.originalData
+    .filter(item => {
+      const word = `${item.id}-${item.title}-${item.price}`;
+      return word.toLowerCase().includes(query.toLowerCase())
+    });
     this.data.next(newProducts);
   }
 
