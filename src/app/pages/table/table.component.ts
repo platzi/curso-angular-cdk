b34210913f881eb75f96a39807ea771e3a32ca3c
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-20 16:05:28 -0400

    :construction: Work in styles

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -10,7 +10,8 @@ import { Product } from '../../models/product.model';
 export class TableComponent implements OnInit {
 
   products: Product[] = [];
-  columns: string[] = ['cover', '#No', 'Name', 'price'];
+  columns: string[] = ['#No', 'Name', 'price', 'cover'];
+  total = 0;
 
   constructor(
     private http: HttpClient
@@ -20,6 +21,9 @@ export class TableComponent implements OnInit {
     this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
     .subscribe(data => {
       this.products = data;
+      this.total = this.products
+        .map(item => item.price)
+        .reduce((price, total) => price + total, 0);
     })
   }
 
